97f4aca477883d124a8d2ad80272b4e3a5807daa
Davi Roque <sowdavi@gmail.com>
2022-07-19 03:27:53 -0300

    button styled

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,8 +7,14 @@ type Props = IButtonProps & {
 
 export function Button({title,...rest}:Props) {
   return (
-    <ButtonNativeBase {...rest}>
-        <Heading>{title}</Heading>
+    <ButtonNativeBase 
+    bg="green.700"
+    h={14}
+    fontSize="sm"
+    rounded="sm"
+    _pressed={{bg:"green.500"}}
+    {...rest}>
+        <Heading color="white" fontSize="sm">{title}</Heading>
     </ButtonNativeBase>
   );
 }
