3a7fdf67fa20873be670f35dd8617fdfc1bc2c62
Davi Roque <sowdavi@gmail.com>
2022-07-26 00:53:00 -0300

    integration bd firestore

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,34 +1,27 @@
 import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
 import { SignOut } from 'phosphor-react-native'
 import { ChatTeardropText } from 'phosphor-react-native'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native'
 import Logo from '../assets/logo_secondary.svg'
 import { Filter } from '../components/Filter'
 import { Button } from '../components/Button'
 import { Order, OrderProps } from '../components/Order'
 import auth from '@react-native-firebase/auth'
+import firestore from '@react-native-firebase/firestore'
 import { Alert } from 'react-native'
 
+import { dateFormat } from '../utils/firestoreDateFormat'
+import {Loading} from '../components/Loading'
+import { isLoading } from 'expo-font';
+
 export function Home() {
+    const [isLoading, setIsLoading] = useState(true)
     const { colors } = useTheme();
     const navigation = useNavigation();
 
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open')
-    const [orders, setOrders] = useState<OrderProps[]>([
-        {
-            id: '123',
-            patrimony: '123456',
-            when: '18/07/2022 ás 14:00',
-            status: 'open'
-        },
-        {
-            id: '343',
-            patrimony: '223456',
-            when: '18/07/2022 ás 14:00',
-            status: 'open'
-        },
-    ])
+    const [orders, setOrders] = useState<OrderProps[]>([])
 
     function handleNewOrder() {
         navigation.navigate('new')
@@ -47,6 +40,31 @@ export function Home() {
             })
     }
 
+    useEffect(() => {
+        setIsLoading(true)
+
+        const subscriber = firestore()
+            .collection('orders')
+            .where('status', '==', statusSelected)
+            .onSnapshot(snapshot => {
+                const data = snapshot.docs.map(doc => {
+                    const { patrimony, description, status, created_at } = doc.data()
+
+                    return {
+                        id: doc.id,
+                        patrimony,
+                        description,
+                        status,
+                        when: dateFormat(created_at)
+                    }
+                })
+                setOrders(data);
+                setIsLoading(false)
+
+            })
+
+        return subscriber
+    }, [statusSelected])
     return (
         <VStack flex={1} pb={6} bg="gray.700">
             <HStack
@@ -90,7 +108,8 @@ export function Home() {
                     />
 
                 </HStack>
-
+                {
+                    isLoading ? <Loading/> :
                 <FlatList
                     data={orders}
                     keyExtractor={item => item.id}
@@ -107,6 +126,7 @@ export function Home() {
                         </Center>
                     )}
                 />
+                    }
                 <Button title='Nova solicitação' onPress={handleNewOrder} />
             </VStack>
 
