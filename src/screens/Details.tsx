5e55b7e3cc6e4341ad01e4f5e706b35b53f60873
Davi Roque <sowdavi@gmail.com>
2022-07-26 00:52:39 -0300

    details integration firestore

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,23 +1,102 @@
-import { VStack, Text } from 'native-base';
+import { useEffect, useState } from 'react';
+import { VStack, HStack, useTheme, Text, ScrollView } from 'native-base';
 import { useRoute } from '@react-navigation/native'
-import {Header} from '../components/Header'
+import { Header } from '../components/Header'
+import { OrderProps } from '../components/Order';
+import firestore from '@react-native-firebase/firestore'
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
+import { dateFormat } from '../utils/firestoreDateFormat';
+import { Loading } from '../components/Loading'
+import { CircleWavyCheck, Hourglass, DesktopTower, Clipboard } from 'phosphor-react-native';
+import { CardDetails } from '../components/CardDetails'
 
-type RouteParams ={
+
+type RouteParams = {
   orderId: string
+}
+
+type OrderDetails = OrderProps & {
+  description: string;
+  solution: string;
+  closed: string;
 }
 
 
 export function Details() {
+  const [order, setOrder] = useState<OrderDetails>({} as OrderDetails)
+  const [isLoading, setIsLoading] = useState(true)
+  const [solution, setSolution] = useState(true)
+
+  const { colors } = useTheme();
 
   const route = useRoute();
-  const {orderId} = route.params as RouteParams;
+  const { orderId } = route.params as RouteParams;
+
+  useEffect(() => {
+    firestore()
+      .collection<OrderFirestoreDTO>('orders')
+      .doc(orderId)
+      .get()
+      .then((doc) => {
+        const { patrimony, description, status, created_at, closed_at, solution } = doc.data()
+
+        const closed = closed_at ? dateFormat(closed_at) : null;
+
+        setOrder({
+          id: doc.id,
+          patrimony,
+          description,
+          status,
+          solution,
+          when: dateFormat(created_at),
+          closed
+        });
+        setIsLoading(false);
+      })
+  }, []);
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <VStack flex={1} bg="gray.700">
-        <Header title="solicitação"/>
-        <Text color="white">
-          {orderId}
+      <Header title="solicitação" />
+      <HStack bg="gray.500" justifyContent="center" p={4}>
+        {
+          order.status === 'closed' ?
+            <CircleWavyCheck size={22} color={colors.green[300]} /> :
+            <Hourglass size={22} color={colors.green[300]} />
+        }
+        <Text fontSize="sm" color={order.status === 'closed' ? colors.green[300] :
+          colors.secondary[700]}
+          ml={2}
+          textTransform="uppercase"
+        >
+          {order.status === 'closed' ? 'finalizado' : 'em andamento'}
         </Text>
+
+      </HStack>
+      <ScrollView mx={5} showsVerticalScrollIndicator={false}>
+        <CardDetails
+          title="equipamento"
+          description={`Patrimônio ${order.patrimony}`}
+          icon={DesktopTower}
+          footer={order.when}
+        />
+        <CardDetails
+          title="Descrição do problema"
+          description={`Patrimônio ${order.description}`}
+          icon={Clipboard}
+        />
+        <CardDetails
+          title="Solução"
+          icon={CircleWavyCheck}
+          footer={order.closed && `Encerrado em ${order.closed}`}
+        >
+
+        </CardDetails>
+      </ScrollView>
     </VStack>
   );
 }
